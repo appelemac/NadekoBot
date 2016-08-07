b196d664257a7609eb5eee10f78ca8a299883e9c
appelemac <appelemac@gmail.com>
2016-08-07 20:29:19 +0200

    Default stuff

---
+++ NadekoBot/Models/Credentials.cs
@@ -0,0 +1,17 @@
+﻿namespace NadekoBot.Models
+{
+    public class Credentials
+    {
+        public string Token { get; set; } = "";
+        public string ClientId { get; set; } = "170254782546575360";
+        public ulong BotId { get; set; } = 1231231231231;
+        public ulong[] OwnerIds { get; set; } = { 123123123123, 5675675679845 };
+        public string GoogleAPIKey { get; set; } = "";
+        public string SoundCloudClientID { get; set; } = "";
+        public string MashapeKey { get; set; } = "";
+        public string LOLAPIKey { get; set; } = "";
+        public string TrelloAppKey { get; set; } = "";
+        public string CarbonKey { get; set; } = "";
+        public string OsuAPIKey { get; set; } = "";
+    }
+}
---
+++ NadekoBot/NadekoClient.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadekoBot
+{
+    public class NadekoClient
+    {
+        public static Discord.API.DiscordSocketApiClient SocketClient { get; private set; }
+        public static Models.Credentials Creds { get; set; }
+        public static string BotMention { get; set; } = "";
+
+
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            InitializeCredentials();
+            
+
+        }
+
+        private static void InitializeCredentials()
+        {
+            try
+            {
+                File.WriteAllText("credentials_example.json", JsonConvert.SerializeObject(new Models.Credentials(), Formatting.Indented));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(Strings.NadekoClient_Main_WritingExampleOfCredentialsFailed0, e.Message);
+            }
+            try
+            {
+                Creds = JsonConvert.DeserializeObject<Models.Credentials>(File.ReadAllText("credentials.json"))
+            } catch (Exception e)
+            {
+                Console.WriteLine("Could not initialize Credentials from credentials.json: {0}, quitting...", e.Message);
+                Console.Read();
+                Environment.Exit(1);
+            }
+        }
+    }
+}
