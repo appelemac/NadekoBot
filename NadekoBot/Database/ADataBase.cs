39df1f059b7a357e9e661c2dc915cad7557b4a29
appelemac <appelemac@gmail.com>
2016-08-08 23:03:11 +0200

    renaming

---
+++ NadekoBot/Database/ADataBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NadekoBot.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+namespace NadekoBot.Database
+{
+    
+    /// <summary>
+    /// Handles interaction with Databases
+    /// </summary>
+    public abstract class ADataBase : DbContext
+    {
+        public DbSet<CommandModel> Commands { get; set; }
+        public DbSet<ServerModel> Servers { get; set; }
+        
+        protected abstract override void OnConfiguring(DbContextOptionsBuilder optionsBuilder);
+
+    }
+
+}
